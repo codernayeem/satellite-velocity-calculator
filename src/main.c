b2876f5a6a6628f6c8c1fdc93c09b29447c4eb5c
Md Nayeem <codernayeem898@gmail.com>
2020-06-17 01:29:44 +0600

    Added main calculation

--- src/main.c
+++ src/main.c
@@ -5,6 +5,20 @@ main()
 {
     printf("\n\t***  Satellite Velocity Calculator  ***\n");
 
+    double height, v, t;
+
+    printf("\n [+] - Height(km) of the satellite from earth's surface : ");
+    scanf("%lf", &height);
+
+    // taking height to meters
+    height = height * 1000;
+
+    v = sqrt(9.8*(6400000+height)) / 1000;
+    printf("\nVelocity         : %lf km/s\n", v);
+
+    t = 2*3.1416*sqrt(pow(6400000+height, 3)/(9.8*6400000*6400000))/3600;
+    printf("\nRotation period  : %lf Hours\n", t);
+
     getch();
 	return 0;
 }
